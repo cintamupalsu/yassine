bf5e08bc17b31b7ae47058e95c02cd684fa8e89b
a_maulana <cintamupalsu@hotmail.com>
2018-01-08 15:32:14 +0900

    pull out Japanese Translation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mighty-lowlands-14253.herokuapp.com'
+  host = 'scary-pirate-19006.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
